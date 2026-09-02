cd70164233eb855725266fe94540b2f800d99a7e
WoochangJin <jwcgomunamu@gmail.com>
2026-09-02 20:40:37 +0900

    Make Basic Modules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod config;
+mod state;
+mod error;
+mod broker;
+mod dto;
+mod handlers;
+mod models;
+mod notification;
+mod repositories;
+mod risk;
+mod routes;
+mod scheduler;
+mod services;
+mod strategy;
+mod ws;
+
 fn main() {
     println!("Hello, world!");
 }
